890ce9fe49418ebd2681cffd979bd0bb36c4ed07
Екатерина <90092671+belkatya@users.noreply.github.com>
2025-06-03 22:24:01 +0300

    Update application.js

--- 1/src/application.js
+++ 1/src/application.js
@@ -1,3 +1,20 @@
 // BEGIN
-
+export default () => {
+    const form = document.querySelector('form');
+    const input = form.querySelector('input[name="number"]');
+    const result = document.getElementById('result');
+    const reset = document.querySelector('button[type="button"]');
+    input.focus();
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        result.textContent = parseInt(result.textContent, 10) + parseInt(input.value, 10);
+        form.reset();
+        input.focus();
+    });
+    reset.addEventListener('click', () => {
+        result.textContent = 0;
+        form.reset();
+        input.focus();
+    });
+}
 // END
